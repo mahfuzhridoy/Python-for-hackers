a1445048c6b4f2b5962ee84294807bd1d4553559
Mahfuzur Rahman <mahfuz291199@gmail.com>
2026-09-04 10:43:41 +0600

    url_resolver speed improved. Threads were added

--- url_Resolver/resolver.py
+++ url_Resolver/resolver.py
@@ -1,36 +1,49 @@
+#!/usr/bin/env python3
 import sys
 import requests
+from concurrent.futures import ThreadPoolExecutor  # For creating multiple threads
+import argparse
 
-session = requests.Session()
+session = requests.Session() # session creation
 session.headers.update({
     "User-Agent": "Mozilla/5.0"
 })
-resolved = set()
-# headers = {
-#     "User-Agent": "Mozilla/5.0"
-# }
 
-def resolver(urls):
-    for url in urls:
-        if not url:
-            continue
-        url = url.strip()
-        try:
-            response = session.get(url,
-                                    # headers=headers,
-                                    timeout=2
-                                    )
-        except requests.RequestException:
-            continue
 
-        if response.status_code == 200:
-            resolved.add(url)
+def resolver(url):
+    url = url.strip()
+    try:
+        response = session.get(url,
+                                timeout=2
+                                )
+    except requests.RequestException:
+        return
+
+    if response.status_code == 200:
+        print(url)
 
 def main():
-    urls = sys.stdin
-    resolver(urls)
-    for r in sorted(resolved):
-        print(r)
+    urls = sys.stdin.readlines()
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--threads",
+                        "-t",
+                        type=int,
+                        default=50,
+                        help="Number of concurrent requests to be sent. default treads 50"
+)
+    args = parser.parse_args()
+
+    threads = args.threads
+
+    # Creating multiple threads.
+
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        for url in urls:
+            executor.submit(resolver, url)
+    
 
 if __name__ == "__main__":
     main()
